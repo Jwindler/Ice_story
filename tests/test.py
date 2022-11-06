efc8affe9f16190ea7e724d65dff09a0fc43a932
Swindler <jzj@1033199817@qq.com>
2022-11-06 16:05:10 +0800

    fix .gitignore

---
+++ tests/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Swindler
+@contact: jzjlab@163.com
+@file: test.py
+@time: 2022/11/6 15:21
+@function:
+"""
+
+
+def main():
+    pass
+
+
+class Main:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
